2ba7cb88b61a3637c27226a79a4dcee405e6a598
Paul Ward <asmodai@enterprise-dev.home>
2016-08-16 21:59:26 +0100

    More files.

---
+++ install.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# -*- Mode: Shell-script -*-
+#
+# install.sh --- Dot files installer.
+#
+# Copyright (c) 2016 Paul Ward <asmodai@gmail.com>
+#
+# Author:     Paul Ward <asmodai@gmail.com>
+# Maintainer: Paul Ward <asmodai@gmail.com>
+# Created:    16 Aug 2016 21:31:24
+# Keywords:   
+# URL:        not distributed yet
+#
+# {{{ License:
+#
+# This program is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY  WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# }}}
+# {{{ Commentary:
+#
+# }}}
+
+TRUE="true"
+FALSE="false"
+
+_rootwd=`pwd`
+
+if [[ ! -f install.sh ]];
+then
+    echo "Please run this sript from inside the dotfiles folder."
+    exit 1
+fi
+
+yesOrNo() {
+    local msg="$1"
+
+    echo -n "${msg}? [Y/n] "
+
+    while true;
+    do
+        read -N 1 input
+        echo
+
+        case ${input} in
+            [yY]) YESORNO=$TRUE  ; return ;;
+            [nN]) YESORNO=$FALSE ; return ;;
+        esac
+    done
+}
+
+installZsh() {
+    yesOrNo "Install ZSH doftiles"
+
+    if [[ "${YESORNO}" = "${TRUE}" ]];
+    then
+        echo "Installing ZSH dotfiles..."
+
+        rm ${HOME}/.zshrc 2>/dev/null
+        ln -s ${_rootwd}/zshrc.d/zshrc ${HOME}/.zshrc
+
+        echo "Done."
+    fi
+}
+
+installPowerLine() {
+    yesOrNo "Install PowerLine config"
+
+    if [[ "${YESORNO}" = "${TRUE}" ]];
+    then
+        echo "Installing PowerLine config..."
+
+        rm -rf ${HOME}/.config/powerline 2>/dev/null
+        ln -s ${_rootwd}/powerline ${HOME}/.config/powerline
+
+        echo "Done."
+    fi
+}
+
+installZsh
+installPowerLine
+
+
+
+# install.sh ends here.
