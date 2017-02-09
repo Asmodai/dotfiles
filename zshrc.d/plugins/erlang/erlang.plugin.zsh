4ae0c087b65d6023a4de326e65f409fe449a7795
Paul Ward <asmodai@gmail.com>
2017-02-09 20:21:55 +0000

    Added Erlang and Git plugins.

---
+++ zshrc.d/plugins/erlang/erlang.plugin.zsh
@@ -0,0 +1,10 @@
+if [ -d ~/erlang ]
+then
+  alias erlang16="source ~/erlang/R16/activate"
+  alias erlang18="source ~/erlang/R18/activate"
+  alias erlang19="source ~/erlang/R19/activate"
+
+  # Set the default
+  source ~/erlang/R16/activate
+fi
+
