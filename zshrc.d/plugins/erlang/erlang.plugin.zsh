b4af948b9f344fc6746d8d550d9bd7be1111b990
Paul Ward <asmodai@gmail.com>
2017-03-08 01:57:46 +0000

    Updated so it's more portable.

--- zshrc.d/plugins/erlang/erlang.plugin.zsh
+++ zshrc.d/plugins/erlang/erlang.plugin.zsh
@@ -1,10 +1,20 @@
 if [ -d ~/erlang ]
 then
-  alias erlang16="source ~/erlang/R16/activate"
-  alias erlang18="source ~/erlang/R18/activate"
-  alias erlang19="source ~/erlang/R19/activate"
+  if [ -d ~/erlang/R16 ]
+  then
+    alias erlang16="source ~/erlang/R16/activate"
+  fi
 
-  # Set the default
-  source ~/erlang/R16/activate
+  if [ -d ~/erlang/R18 ]
+  then
+    alias erlang18="source ~/erlang/R18/activate"
+  fi
+
+  # R19 is the default.
+  if [ -d ~/erlang/R19 ]
+  then
+    alias erlang19="source ~/erlang/R19/activate"
+    source ~/erlang/R19/activate
+  fi
 fi
 
