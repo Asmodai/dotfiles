51907afbf2b1e5bf5f7efb3cfb37c613e53b7198
Paul Ward <asmodai@gmail.com>
2022-08-01 22:41:25 +0100

    Fix GNUstep plugin.

--- zshrc.d/plugins/gnustep/gnustep.plugin.zsh
+++ zshrc.d/plugins/gnustep/gnustep.plugin.zsh
@@ -1,5 +1,8 @@
 # Set this to your GNUstep library directory
 export GSTEP_LIBRARY_PATH=/usr/GNUstep/System/Library/Makefiles
 
-source ${GSTEP_LIBRARY_PATH}/GNUstep.sh
+if [ -d ${GSTEP_LIBRARY_PATH} ]
+then
+    source ${GSTEP_LIBRARY_PATH}/GNUstep.sh
+fi
 
