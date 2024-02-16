66d2d2089634bf14e1c6fb290efb2cbde5df50cc
Paul Ward <asmodai@gmail.com>
2024-02-16 18:38:46 +0000

    Ugh, ok, use modified old theme
    Because it still sucks.

--- zshrc.d/plugins/gtk/gtk.plugin.zsh
+++ zshrc.d/plugins/gtk/gtk.plugin.zsh
@@ -2,11 +2,6 @@ if [ -d /usr/share/themes ]
 then
   #export GTK2_RC_FILES=/usr/share/themes/Numix/gtk-2.0/gtkrc
   #export GTK_THEME=Numix:dark
-  #export GTK2_RC_FILES=/home/asmodai/.themes/MyDark/gtk-2.0/gtkrc
-  #export GTK_THEME=MyDark
-
-  # Let KDE handle all this.
-  unset GTK_RC_FILES
-  unset GTK2_RC_FILES
-  unset GTK_THEME
+  export GTK2_RC_FILES=/home/asmodai/.themes/MyDark/gtk-2.0/gtkrc
+  export GTK_THEME=MyDark
 fi
